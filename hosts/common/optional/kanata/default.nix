e013bdb5193147e0c621ad74c8f9f75bc78c1ed9
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-06-24 04:13:26 +0800

    feat: add qwerty

--- hosts/common/optional/kanata/default.nix
+++ hosts/common/optional/kanata/default.nix
@@ -1,15 +1,27 @@
-{ pkgs, ... }: {
-  environment.systemPackages = with pkgs; [
-    kanata
-  ];
+{ pkgs, lib, ... }:
+{
+  environment.systemPackages = with pkgs; [ kanata ];
 
+  systemd.services.kanafa.wantedBy = lib.mkForce [ ];
   services.kanata = {
-    enable = true;
+    enable = lib.mkDefault true;
     keyboards = {
       laptop = {
-      devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
-      configFile = ./colemak.kbd;
-        };
+        devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
+        configFile = ./colemak.kbd;
+      };
     };
+
+    # qwerty.configuration = {
+    #   services.kanata = {
+    #     enable = false;
+    #     keyboards = {
+    #       laptop = {
+    #         devices = [ "/dev/input/by-path/platform-i8042-serio-0-event-kbd" ];
+    #         configFile = ./qwerty.kbd;
+    #       };
+    #     };
+    #   };
+    # };
   };
 }
