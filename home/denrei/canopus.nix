f2f531c5f4fa3f7b3f1f01aba3cbbee21f84bfaa
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-06-24 17:25:51 +0800

    feat: import new settings

--- home/denrei/canopus.nix
+++ home/denrei/canopus.nix
@@ -2,15 +2,17 @@
 {
   imports = [
     #################### Required Configs ####################
-    common/core #required
+    common/core # required
 
     #################### Host-specific Optional Configs ####################
+    common/optional/theme.nix
     common/optional/sops.nix
     common/optional/ags.nix
     common/optional/helper-scripts
 
     common/optional/desktops
     common/optional/browsers
+    common/optional/media
   ];
 
   home = {
@@ -18,4 +20,3 @@
     homeDirectory = "/home/${configVars.username}";
   };
 }
-
