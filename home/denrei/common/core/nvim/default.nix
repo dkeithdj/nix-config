7fb7421fefc87714f9a45fdce12b1fbde89dce99
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-06-25 17:35:32 +0800

    fix: nvim

--- home/denrei/common/core/nvim/default.nix
+++ home/denrei/common/core/nvim/default.nix
@@ -1,11 +1,15 @@
 {
+  config,
   lib,
   pkgs,
   inputs,
+  configVars,
   ...
 }: {
   xdg = {
-    configFile.nvim.source = ../nvim;
+    # configFile.nvim.source = ../nvim;
+    # configFile.nvim.source = config.lib.file.mkOutOfStoreSymlink ../nvim;
+    configFile.nvim.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/home/${configVars.username}/common/core/nvim";
     desktopEntries."nvim" = lib.mkIf pkgs.stdenv.isLinux {
       name = "NeoVim";
       comment = "Edit text files";
