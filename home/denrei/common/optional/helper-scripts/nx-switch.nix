318cd4397202b75acc860a10aaad221d8bd59807
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-06-25 17:54:35 +0800

    add nix-switch

---
+++ home/denrei/common/optional/helper-scripts/nx-switch.nix
@@ -0,0 +1,38 @@
+{pkgs, ...}: let
+  symlink =
+    pkgs.writeShellScript
+    /*
+    bash
+    */
+    "symlink" ''
+      if [[ "$1" == "-r" ]]; then
+        rm -rf "$HOME/.config/nvim"
+        rm -rf "$HOME/.config/ags"
+      fi
+
+      if [[ "$1" == "-a" ]]; then
+        rm -rf "$HOME/.config/nvim"
+        rm -rf "$HOME/.config/ags"
+
+        ln -s "$HOME/nix-config/home/denrei/common/core/nvim/" "$HOME/.config/nvim"
+        ln -s "$HOME/nix-config/home/denrei/optional/core/ags" "$HOME/.config/ags"
+      fi
+    '';
+  nx-switch = pkgs.writeShellScriptBin "nx-switch" ''
+    ${symlink} -r
+    sudo nixos-rebuild switch --flake . --impure $@
+    ${symlink} -a
+  '';
+  nx-boot = pkgs.writeShellScriptBin "nx-boot" ''
+    ${symlink} -r
+    sudo nixos-rebuild boot --flake . --impure $@
+    ${symlink} -a
+  '';
+  nx-test = pkgs.writeShellScriptBin "nx-test" ''
+    ${symlink} -r
+    sudo nixos-rebuild test --flake . --impure $@
+    ${symlink} -a
+  '';
+in {
+  home.packages = [nx-switch nx-boot nx-test];
+}
