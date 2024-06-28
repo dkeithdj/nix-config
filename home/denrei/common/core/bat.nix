459f20eb4f790ba9f4d3669d0d2c9d786a18832b
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-06-28 14:38:49 +0800

    feat: add gh

--- home/denrei/common/core/bat.nix
+++ home/denrei/common/core/bat.nix
@@ -1,20 +1,16 @@
 # https://github.com/sharkdp/bat
 # https://github.com/eth-p/bat-extras
-
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs.bat = {
     enable = true;
-    config = {
-      # Show line numbers, Git modifications and file header (but no grid)
-      style = "numbers,changes,header";
-      theme = "gruvbox-dark";
-    };
+    config.theme = "base16";
     extraPackages = builtins.attrValues {
-      inherit (pkgs.bat-extras)
-
-        batgrep# search through and highlight files using ripgrep
-        batdiff# Diff a file against the current git index, or display the diff between to files
-        batman; # read manpages using bat as the formatter
+      inherit
+        (pkgs.bat-extras)
+        batgrep # search through and highlight files using ripgrep
+        batdiff # Diff a file against the current git index, or display the diff between to files
+        batman
+        ; # read manpages using bat as the formatter
     };
   };
 }
