c9fec0417060b531fe631e119077214cf74b5297
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-06-29 01:36:12 +0800

    feat: add vscode

--- home/denrei/common/core/nvim/lua/plugins/lspconfig.lua
+++ home/denrei/common/core/nvim/lua/plugins/lspconfig.lua
@@ -5,6 +5,7 @@ return {
     opts = {
         ----@type lspconfig.options
         servers = {
+            solc = {},
             nixd = {
                 settings = {
                     nixd = {
