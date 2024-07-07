fd461171d5a37f5fb331a074bfe41f663faaf3a4
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-07-08 00:16:10 +0800

    feat: add fmt

--- home/denrei/common/core/editor.nix
+++ home/denrei/common/core/editor.nix
@@ -3,5 +3,5 @@
     enable = true;
     package = pkgs.vscode.fhsWithPackages (ps: with ps; [rustup zlib openssl.dev pkg-config]);
   };
-  home.packages = with pkgs; [zed-editor];
+  home.packages = with pkgs; [zed-editor nixpkgs-fmt];
 }
