c0971b333abada5421998d128e33db8e53a0d4a8
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-07-07 22:23:54 +0800

    feat: editor

--- home/denrei/common/core/editor.nix
+++ home/denrei/common/core/editor.nix
@@ -3,4 +3,9 @@
     enable = true;
     package = pkgs.vscode.fhsWithPackages (ps: with ps; [rustup zlib openssl.dev pkg-config]);
   };
+  # home.packages = with pkgs; [zed-editor];
+  programs."zed-editor" = {
+    enable = true;
+    package = pkgs.zed-editor;
+  };
 }
