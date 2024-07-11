2a16f7e808361ff51942bef81fc1d4dceec5916f
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-07-12 04:58:10 +0800

    feat: lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,12 @@
 {
   description = "Den's Nix-Config";
   # test
+
+  nixConfig = {
+    extra-substituters = ["https://hyprland.cachix.org"];
+    extra-trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   inputs = {
     #################### Official NixOS and HM Package Sources ####################
 
