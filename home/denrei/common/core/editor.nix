52d187f9ed3c26318f6307b38f46d5f1db17f220
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-07-13 20:27:48 +0800

    feat: six

--- home/denrei/common/core/editor.nix
+++ home/denrei/common/core/editor.nix
@@ -6,33 +6,5 @@
   home.packages = with pkgs; [
     zed-editor
     nixpkgs-fmt
-    git
-    gcc
-    gnumake
-    unzip
-    wget
-    curl
-    tree-sitter
-    ripgrep
-    fd
-    fzf
-    go
-    python3
-    nodejs
-
-    prettierd
-    marksman
-    nil
-    lua-language-server
-    luarocks
-    lua
-    stylua
-    alejandra
-    nodePackages.eslint
-    eslint_d
-    nodePackages.prettier
-    ruff-lsp
-    black
-    pyright
   ];
 }
