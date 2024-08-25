83c3f61a42072b80dc5cd64fa03d96bd3c13b50e
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-08-25 22:16:32 +0800

    feat: do gnome

--- hosts/common/optional/cosmic.nix
+++ hosts/common/optional/cosmic.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
-  services.desktopManager.cosmic.enable = true;
-  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = false;
+  services.displayManager.cosmic-greeter.enable = false;
   # environment.systemPackages = with pkgs; [cosmic-icons];
 
   # environment.systemPackages = [pkgs.drm_info pkgs.cosmic-emoji-picker pkgs.cosmic-tasks];
