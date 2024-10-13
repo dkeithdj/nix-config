2a223b0885530bb774c9dd90f43655233512d43f
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-10-14 00:03:02 +0800

    feat: print config

--- hosts/common/core/printing.nix
+++ hosts/common/core/printing.nix
@@ -4,4 +4,20 @@
     nssmdns4 = true;
     openFirewall = true;
   };
+  services.printing = {
+    listenAddresses = ["*:631"];
+    allowFrom = ["all"];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
+    browsedConf = ''
+      BrowseDNSSDSubTypes _cups,_print
+      BrowseLocalProtocols all
+      BrowseRemoteProtocols all
+      CreateIPPPrinterQueues All
+
+      BrowseProtocols all
+    '';
+    drivers = with pkgs; [gutenprint hplip brlaser];
+  };
 }
