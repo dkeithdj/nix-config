b8fb32fde9658463a968b81b8b5411924cbb899a
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-10-18 01:31:51 +0800

    feat: add solana-cli package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 # You can build these directly using 'nix build .#example'
 {pkgs ? (import <nixpkgs> {}), ...}: rec {
   #################### Packages with external source ####################
+  solana-cli = pkgs.callPackage ./solana-cli {};
 }
