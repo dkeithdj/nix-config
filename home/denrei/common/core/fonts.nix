4ba6109c0884f0e1114d0bc9dcbc4d881170b1f2
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-10-24 21:35:18 +0800

    feat: fonts

--- home/denrei/common/core/fonts.nix
+++ home/denrei/common/core/fonts.nix
@@ -1,9 +1,10 @@
 {pkgs, ...}: {
   fonts.fontconfig.enable = true;
+  font.packages = with pkgs; [
+    (nerdfonts.override {fonts = ["FantasqueSansMono" "JetBrainsMono" "FiraMono"];})
+  ];
   home.packages = [
     pkgs.noto-fonts
-    pkgs.nerdfonts.override
-    {fonts = ["FantasqueSansMono"];}
     # pkgs.nerdfonts # loads the complete collection. look into overide for FiraMono or potentially mononoki
     # pkgs.nerdfonts.override
     # { fonts = [ "FantasqueSansMono" ]; }
