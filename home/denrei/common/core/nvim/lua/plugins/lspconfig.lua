1fd49ce7a33ed4556c341a0c9dbbdf5ac3c411e6
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-10-26 17:12:32 +0800

    feat: add lspconfig deno

--- home/denrei/common/core/nvim/lua/plugins/lspconfig.lua
+++ home/denrei/common/core/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,4 @@
+local nvim_lsp = require("lspconfig")
 return {
     -- add pyright to lspconfig
     "neovim/nvim-lspconfig",
@@ -46,6 +47,13 @@ return {
                         },
                     },
                 },
+            },
+            denols = {
+                filetypes = { "typescript", "typescriptreact" },
+                root_dir = function(...) return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...) end,
+            },
+            vtsls = {
+                root_dir = nvim_lsp.util.root_pattern("package.json"),
             },
             -- tsserver = {
             --     -- root_dir = function(...)
