73ada2d65ba5ccf9269fa225637e6969f005649b
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-12-09 03:09:40 +0800

    feat: format colorscheme

--- home/denrei/common/core/nvim/lua/plugins/colorscheme.lua
+++ home/denrei/common/core/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,5 @@
 return {
-  "folke/tokyonight.nvim",
-  vscode = false,
-  lazy = true,
-  opts = { style = "night" },
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "night" },
 }
