2b31ccdbd6ad69876dd2285c0ee75f852c782530
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-12-25 17:47:18 +0800

    feat: add keymap.json

--- home/denrei/common/core/zed/default.nix
+++ home/denrei/common/core/zed/default.nix
@@ -5,9 +5,17 @@
   configVars,
   ...
 }: {
-  xdg.configFile."zed/settings.json".source = lib.mkForce (
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nix-config/home/${configVars.username}/common/core/zed/settings.json"
-  );
+  # xdg.configFile."zed/settings.json".source = lib.mkForce (
+  #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nix-config/home/${configVars.username}/common/core/zed/settings.json"
+  # );
+  xdg.configFile = {
+    "zed/settings.json".source = lib.mkForce (
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nix-config/home/${configVars.username}/common/core/zed/settings.json"
+    );
+    "zed/keymap.json".source = lib.mkForce (
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nix-config/home/${configVars.username}/common/core/zed/keymap.json"
+    );
+  };
 
   programs.zed-editor = {
     enable = true;
