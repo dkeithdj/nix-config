b53310f737b3607964075f3861abd5b772504449
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-12-26 17:09:13 +0800

    fix: options

--- snowfall/modules/nixos/apps/zoom-us/default.nix
+++ snowfall/modules/nixos/apps/zoom-us/default.nix
@@ -10,7 +10,7 @@ with lib;
 with lib.${namespace}; let
   cfg = config.${namespace}.apps.zoom-us;
 in {
-  options.${namespace}.apps.hey = with types; {
+  options.${namespace}.apps.zoom-us = with types; {
     enable = mkBoolOpt false "Whether or not to enable zoom-us.";
   };
 
