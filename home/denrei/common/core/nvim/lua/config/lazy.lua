25d2da7cdd308adbaa7c64af4195a511fb082124
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-12-31 16:11:42 +0800

    feat: nvim config

--- home/denrei/common/core/nvim/lua/config/lazy.lua
+++ home/denrei/common/core/nvim/lua/config/lazy.lua
@@ -1,77 +1,80 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = {
-        { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
-        -- { import = "lazyvim.plugins.extras.lang.nushell" },
+    -- { import = "lazyvim.plugins.extras.lang.nushell" },
 
-        -- { import = "lazyvim.plugins.extras.coding.codeium" },
-        { import = "lazyvim.plugins.extras.ai.copilot" },
-        { import = "lazyvim.plugins.extras.coding.mini-surround" },
-        { import = "lazyvim.plugins.extras.coding.luasnip" },
-        { import = "lazyvim.plugins.extras.editor.leap" },
+    -- { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.editor.leap" },
 
-        -- { import = "lazyvim.plugins.extras.dap.core" },
-        -- { import = "lazyvim.plugins.extras.dap.nlua" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
+    -- { import = "lazyvim.plugins.extras.dap.nlua" },
 
-        -- { import = "lazyvim.plugins.extras.formatting.black" },
-        { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- { import = "lazyvim.plugins.extras.formatting.black" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
 
-        { import = "lazyvim.plugins.extras.lang.astro" },
-        { import = "lazyvim.plugins.extras.lang.clangd" },
-        { import = "lazyvim.plugins.extras.lang.docker" },
-        { import = "lazyvim.plugins.extras.lang.elixir" },
-        { import = "lazyvim.plugins.extras.lang.git" },
-        { import = "lazyvim.plugins.extras.lang.go" },
-        { import = "lazyvim.plugins.extras.lang.json" },
-        { import = "lazyvim.plugins.extras.lang.markdown" },
-        { import = "lazyvim.plugins.extras.lang.nix" }, -- use nixd
-        -- { import = "lazyvim.plugins.extras.lang.omnisharp" },
-        { import = "lazyvim.plugins.extras.lang.python" },
-        { import = "lazyvim.plugins.extras.lang.rust" },
-        { import = "lazyvim.plugins.extras.lang.svelte" },
-        { import = "lazyvim.plugins.extras.lang.tailwind" },
-        { import = "lazyvim.plugins.extras.lang.yaml" },
-        { import = "lazyvim.plugins.extras.lang.typescript" },
-        { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.astro" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.elixir" },
+    { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.nix" }, -- use nixd
+    -- { import = "lazyvim.plugins.extras.lang.omnisharp" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.svelte" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
 
-        { import = "lazyvim.plugins.extras.linting.eslint" },
-        { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
 
-        { import = "lazyvim.plugins.extras.vscode" },
-        { import = "plugins" },
+    { import = "lazyvim.plugins.extras.vscode" },
+    { import = "plugins" },
+  },
+  lockfile = vim.fn.stdpath("cache") .. "/lazy-lock.json",
+  defaults = {
+    lazy = false,
+    version = false, -- always use the latest git commit
+  },
+  checker = { enabled = false }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
     },
-    lockfile = vim.fn.stdpath("cache") .. "/lazy-lock.json",
-    defaults = {
-        lazy = false,
-        version = false, -- always use the latest git commit
-    },
-    checker = { enabled = false }, -- automatically check for plugin updates
-    performance = {
-        rtp = {
-            -- disable some rtp plugins
-            disabled_plugins = {
-                "gzip",
-                -- "matchit",
-                -- "matchparen",
-                -- "netrwPlugin",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
+  },
 })
--- home/denrei/common/core/nvim/lua/plugins/lspconfig.lua
+++ home/denrei/common/core/nvim/lua/plugins/lspconfig.lua
@@ -1,90 +1,118 @@
 local nvim_lsp = require("lspconfig")
+
+local lsp = vim.g.lazyvim_python_lsp or "pylsp"
+local ruff = vim.g.lazyvim_python_ruff or "ruff"
 return {
-    -- add pyright to lspconfig
+  {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     opts = {
-        ----@type lspconfig.options
-        servers = {
-            -- nginx_language_server = {},
-            solc = {},
-            -- nixd = {
-            --     settings = {
-            --         nixd = {
-            --             formatting = {
-            --                 command = { "alejandra" },
-            --             },
-            --         },
-            --     },
-            -- },
-            -- pyright will be automatically installed with mason and loaded with lspconfig
-            -- pyright = {},
-            -- lua_ls = {
-            --     settings = {
-            --         Lua = {
-            --             runtime = {
-            --                 version = "LuaJIT",
-            --             },
-            --             diagnostics = {
-            --                 globals = { "use", "vim" },
-            --             },
-            --             hint = {
-            --                 enable = true,
-            --                 setType = true,
-            --             },
-            --             telemetry = {
-            --                 enable = false,
-            --             },
-            --             workspace = {
-            --                 library = {
-            --                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-            --                     [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-            --                     [vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types"] = true,
-            --                     [vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
-            --                 },
-            --                 maxPreload = 100000,
-            --                 preloadFileSize = 10000,
-            --             },
-            --         },
-            --     },
-            -- },
-            denols = {
-                filetypes = { "typescript", "typescriptreact" },
-                root_dir = function(...) return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...) end,
+      ----@type lspconfig.options
+      servers = {
+        -- nginx_language_server = {},
+        solc = {},
+        -- nixd = {
+        --     settings = {
+        --         nixd = {
+        --             formatting = {
+        --                 command = { "alejandra" },
+        --             },
+        --         },
+        --     },
+        -- },
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pyflakes = { enabled = false },
+                pycodestyle = { enabled = false },
+                autopep8 = { enabled = false },
+                yapf = { enabled = false },
+                mccabe = { enabled = false },
+                pylsp_mypy = { enabled = false },
+                pylsp_black = { enabled = false },
+                pylsp_isort = { enabled = false },
+              },
             },
-            vtsls = {
-                root_dir = nvim_lsp.util.root_pattern("package.json"),
-            },
-            -- tsserver = {
-            --     -- root_dir = function(...)
-            --     --   return require("lspconfig.util").root_pattern(".git")(...)
-            --     -- end,
-            --     single_file_support = false,
-            --     settings = {
-            --         typescript = {
-            --             inlayHints = {
-            --                 includeInlayParameterNameHints = "literal",
-            --                 includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            --                 includeInlayFunctionParameterTypeHints = false,
-            --                 includeInlayVariableTypeHints = false,
-            --                 includeInlayPropertyDeclarationTypeHints = true,
-            --                 includeInlayFunctionLikeReturnTypeHints = true,
-            --                 includeInlayEnumMemberValueHints = true,
-            --             },
-            --         },
-            --         javascript = {
-            --             inlayHints = {
-            --                 includeInlayParameterNameHints = "all",
-            --                 includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-            --                 includeInlayFunctionParameterTypeHints = true,
-            --                 includeInlayVariableTypeHints = true,
-            --                 includeInlayPropertyDeclarationTypeHints = true,
-            --                 includeInlayFunctionLikeReturnTypeHints = true,
-            --                 includeInlayEnumMemberValueHints = true,
-            --             },
-            --         },
-            --     },
-            -- },
+          },
         },
+        -- lua_ls = {
+        --     settings = {
+        --         Lua = {
+        --             runtime = {
+        --                 version = "LuaJIT",
+        --             },
+        --             diagnostics = {
+        --                 globals = { "use", "vim" },
+        --             },
+        --             hint = {
+        --                 enable = true,
+        --                 setType = true,
+        --             },
+        --             telemetry = {
+        --                 enable = false,
+        --             },
+        --             workspace = {
+        --                 library = {
+        --                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        --                     [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+        --                     [vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types"] = true,
+        --                     [vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
+        --                 },
+        --                 maxPreload = 100000,
+        --                 preloadFileSize = 10000,
+        --             },
+        --         },
+        --     },
+        -- },
+        denols = {
+          filetypes = { "typescript", "typescriptreact" },
+          root_dir = function(...) return nvim_lsp.util.root_pattern("deno.jsonc", "deno.json")(...) end,
+        },
+        vtsls = {
+          root_dir = nvim_lsp.util.root_pattern("package.json"),
+        },
+        -- tsserver = {
+        --     -- root_dir = function(...)
+        --     --   return require("lspconfig.util").root_pattern(".git")(...)
+        --     -- end,
+        --     single_file_support = false,
+        --     settings = {
+        --         typescript = {
+        --             inlayHints = {
+        --                 includeInlayParameterNameHints = "literal",
+        --                 includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        --                 includeInlayFunctionParameterTypeHints = false,
+        --                 includeInlayVariableTypeHints = false,
+        --                 includeInlayPropertyDeclarationTypeHints = true,
+        --                 includeInlayFunctionLikeReturnTypeHints = true,
+        --                 includeInlayEnumMemberValueHints = true,
+        --             },
+        --         },
+        --         javascript = {
+        --             inlayHints = {
+        --                 includeInlayParameterNameHints = "all",
+        --                 includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        --                 includeInlayFunctionParameterTypeHints = true,
+        --                 includeInlayVariableTypeHints = true,
+        --                 includeInlayPropertyDeclarationTypeHints = true,
+        --                 includeInlayFunctionLikeReturnTypeHints = true,
+        --                 includeInlayEnumMemberValueHints = true,
+        --             },
+        --         },
+        --     },
+        -- },
+      },
     },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local servers = { "pyright", "pylsp", "ruff", "ruff_lsp", ruff, lsp }
+      for _, server in ipairs(servers) do
+        opts.servers[server] = opts.servers[server] or {}
+        opts.servers[server].enabled = server == lsp or server == ruff
+      end
+    end,
+  },
 }
