fca27d84dd680da79bb39cb7432392e5e5342cdc
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2025-02-15 01:41:46 +0800

    feat: disable zellij

--- home/denrei/common/core/zellij/default.nix
+++ home/denrei/common/core/zellij/default.nix
@@ -3,10 +3,12 @@
   pkgs,
   configVars,
   ...
-}: {
-  xdg.configFile."zellij/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nix-config/home/${configVars.username}/common/core/zellij/config.kdl";
+}:
+{
+  xdg.configFile."zellij/config.kdl".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nix-config/home/${configVars.username}/common/core/zellij/config.kdl";
   programs.zellij = {
-    enable = true;
+    enable = false;
     package = pkgs.zellij;
     # enableBashIntegration = true;
     # enableZshIntegration = true;
