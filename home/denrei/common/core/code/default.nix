ba69a129dfe119b1781d4574f1053322ccc5bc82
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2025-02-16 15:53:22 +0800

    feat: code

--- home/denrei/common/core/code/default.nix
+++ home/denrei/common/core/code/default.nix
@@ -1,5 +1,20 @@
-{ pkgs, ... }:
 {
+  pkgs,
+  lib,
+  config,
+  configVars,
+  ...
+}:
+{
+
+  xdg.configFile = {
+    "Code/User/settings.json".source = lib.mkForce (
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nix-config/home/${configVars.username}/common/core/code/settings.json"
+    );
+    "Code/User/keybindings.json".source = lib.mkForce (
+      config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Projects/nix-config/home/${configVars.username}/common/core/code/keybindings.json"
+    );
+  };
   programs.vscode = {
     enable = true;
     package = pkgs.vscode.fhsWithPackages (
