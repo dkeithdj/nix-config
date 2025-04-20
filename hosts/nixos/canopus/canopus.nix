fbab5357e6872795ff014a9b0b35850067bbdfcd
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2025-04-20 20:36:29 +0800

    feat: build machine
    fix: typo

--- hosts/nixos/canopus/canopus.nix
+++ hosts/nixos/canopus/canopus.nix
@@ -1,4 +1,23 @@
 { ... }:
 {
-  nix.buildMachines = true;
+  nix.buildMachines = [
+    {
+      hostName = "altair";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      maxJobs = 1;
+      speedFactor = 2;
+      supportedFeatures = [
+        "nixos-test"
+        "benchmark"
+        "big-parallel"
+        "kvm"
+      ];
+      mandatoryFeatures = [ ];
+    }
+  ];
+  nix.distributedBuilds = true;
+  nix.extraOptions = ''
+    builders-use-substitutes = true
+  '';
 }
